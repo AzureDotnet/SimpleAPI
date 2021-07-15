474ad409aad09b74ecf91b399cf8ea35296de65b
unknown <sandeep29175@gmail.com>
2021-07-15 14:15:32 -0700

    Initial Commit

---
+++ TEST/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using SimpleAPI.Controllers;
+using Xunit;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller=new WeatherForecastController();
+
+        [Fact]
+        public void Test1()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("sandeep karan", returnValue.Value);
+        }
+    }
+}
